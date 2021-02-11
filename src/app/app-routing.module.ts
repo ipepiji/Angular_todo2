f59c8a0fa56f1dc49186559bfd0f99d14e8b2d38
IpePiji <hafizhi.z@okwave.global>
2021-02-12 05:18:35 +0800

    First stage - Half setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,31 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HeaderComponent } from './layouts/header/header.component';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { AddTodosComponent } from './add-todos/add-todos.component';
+import { TodosComponent } from './todos/todos.component';
+import { ExtraComponent } from './extra/extra.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomepageComponent, pathMatch: 'full' },
+  { path: 'about-us', component: AboutUsComponent },
+  
+  { path: '**', redirectTo: '404' },
+  { path: '404', component: NotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const RoutingComponents = [
+  HeaderComponent,
+  HomepageComponent,
+  AboutUsComponent,
+  AddTodosComponent,
+  TodosComponent,
+  ExtraComponent
+]
